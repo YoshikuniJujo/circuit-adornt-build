dcc8a190e4620713462631db41ca886b58ba52c5
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-08-21 12:07:28 +0900

    refactoring interface

--- src/Circuit/Adornt/Builder.hs
+++ src/Circuit/Adornt/Builder.hs
@@ -1,37 +1,22 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Circuit.Adornt.Builder (
-	CircuitBuilder, CBState(..), initCBState,
-	BasicGate(..), gateWires,
+	-- * Circuit Builder
+	CircuitBuilder, CBState, initCBState,
+
+	-- * Basic Gate
+	BasicGate,
 	constGate, idGate, andGate, orGate, notGate, triGate, cheatGate,
-	IWire(..), makeIWire, delay,
-	OWire(..), triIWire, makeOWire, makeOWireTri,
-	Wire11, Wire21, Wire31, Wire41, Wire51, Wire22,
-	connectWire, FromOWire, BitLen, BitPosIn, BitPosOut,
-	connectWire0, connectWire64, connectWire0_64,
-	BlockName, putNamedBlock
-	) where
 
-import Data.Word
+	-- * Input and Output Wire
+	IWire, delay, OWire, Wire11, Wire21, Wire31, Wire41, Wire51, Wire22,
 
-import CircuitBuilder.CircuitCore
+	-- * Wire Connection
+	connectWire, connectWire0, connectWire64, connectWire0_64,
+	BitLen, BitPosIn, BitPosOut,
 
-constGate :: Word64 -> CircuitBuilder OWire
-constGate = constGateW
+	-- * Named Block
+	putNamedBlock, BlockName
+) where
 
-type Wire11 = (IWire, OWire)
-type Wire21 = (IWire, IWire, OWire)
-type Wire31 = (IWire, IWire, IWire, OWire)
-type Wire41 = (IWire, IWire, IWire, IWire, OWire)
-type Wire51 = (IWire, IWire, IWire, IWire, IWire, OWire)
-
-type Wire22 = (IWire, IWire, OWire, OWire)
-
-connectWire64 :: OWire -> IWire -> CircuitBuilder ()
-connectWire64 o i = connectWire (o, 64, 0) (i, 64, 0)
-
-connectWire0 :: OWire -> IWire -> CircuitBuilder ()
-connectWire0 o i = connectWire (o, 1, 0) (i, 1, 0)
-
-connectWire0_64 :: OWire -> IWire -> CircuitBuilder ()
-connectWire0_64 o i = connectWire (o, 1, 0) (i, 64, 0)
+import Circuit.Adornt.BuilderCore
---
+++ src/Circuit/Adornt/BuilderCore.hs
@@ -0,0 +1,47 @@
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Circuit.Adornt.BuilderCore (
+	-- * Circuit Builder
+	CircuitBuilder, CBState(..), FromOWire, initCBState,
+
+	-- * Basic Gate
+	BasicGate(..),
+	constGate, idGate, andGate, orGate, notGate, triGate, cheatGate,
+	gateWires,
+
+	-- * Input and Output Wire
+	IWire(..), makeIWire, delay,
+	OWire(..), triIWire, makeOWire, makeOWireTri,
+	Wire11, Wire21, Wire31, Wire41, Wire51, Wire22,
+
+	-- * Wire Connection
+	connectWire, connectWire0, connectWire64, connectWire0_64,
+	BitLen, BitPosIn, BitPosOut,
+
+	-- * Named Block
+	putNamedBlock, BlockName
+	) where
+
+import Data.Word
+
+import CircuitBuilder.CircuitCore
+
+constGate :: Word64 -> CircuitBuilder OWire
+constGate = constGateW
+
+type Wire11 = (IWire, OWire)
+type Wire21 = (IWire, IWire, OWire)
+type Wire31 = (IWire, IWire, IWire, OWire)
+type Wire41 = (IWire, IWire, IWire, IWire, OWire)
+type Wire51 = (IWire, IWire, IWire, IWire, IWire, OWire)
+
+type Wire22 = (IWire, IWire, OWire, OWire)
+
+connectWire64 :: OWire -> IWire -> CircuitBuilder ()
+connectWire64 o i = connectWire (o, 64, 0) (i, 64, 0)
+
+connectWire0 :: OWire -> IWire -> CircuitBuilder ()
+connectWire0 o i = connectWire (o, 1, 0) (i, 1, 0)
+
+connectWire0_64 :: OWire -> IWire -> CircuitBuilder ()
+connectWire0_64 o i = connectWire (o, 1, 0) (i, 64, 0)
